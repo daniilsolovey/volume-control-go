ea48e7b0e907d04c199061d415ca9581584fb972
daniilsolovey <sda010394@gmail.com>
2020-06-06 21:43:35 +0300

    first commit

---
+++ increase-volume/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"strconv"
+
+	"github.com/itchyny/volume-go"
+)
+
+const (
+	showingTime = "300"
+)
+
+func main() {
+	vol, err := volume.GetVolume()
+	if err != nil {
+		log.Fatalf("get volume failed: %+v", err)
+	}
+	fmt.Printf("current volume: %d\n", vol)
+
+	if vol == 100 {
+		message := "volume: " + strconv.Itoa(vol) + "%"
+		notify := exec.Command("notify-send", "-t", showingTime, "info", message)
+		err = notify.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	err = volume.SetVolume(vol + 10)
+	if err != nil {
+		log.Fatalf("set volume failed: %+v", err)
+	}
+
+	fmt.Printf("set volume success\n")
+
+	message := "volume: " + strconv.Itoa(vol+10) + "%"
+	notify := exec.Command("notify-send", "-t", showingTime, "info", message)
+	err = notify.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
